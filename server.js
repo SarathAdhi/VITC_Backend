46db30d89c5c4229833b318da0e541f2fb8568ff
SarathAdhi <s.sarathadhithya@gmail.com>
2022-12-05 19:05:16 +0530

    minor changes

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get("/", (req, res) => res.send("hello"));
 
 app.use("/faculty", facultyRouter);
